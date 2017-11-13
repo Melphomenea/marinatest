e0b50186c025f5cff0a994ab03d4802894d35f3b
Melphomenea <melphomenea@hotmail.com>
2017-11-13 11:57:07 +0100

    Update AppTest.java

--- src/test/java/com/marinatest/AppTest.java
+++ src/test/java/com/marinatest/AppTest.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions (plugin={"pretty", "html:target/cucumber-html-report"},
-            features={"src/test/resurces"},
+            features={"src/test/resources"},
             glue={"com.marinatest.steps", "com.marinatest.world"})
 public class AppTest {
 }
